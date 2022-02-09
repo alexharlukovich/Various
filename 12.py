409382dfe7db3fef885f9187874475e3514abe3f
Your Name <you@example.com>
2022-02-09 22:47:58 +0100

    tasks 26-34

--- 12.py
+++ 12.py
@@ -339,7 +339,102 @@ print(random.choice(w))
 
 #task 26 Current time
 
-from time import asctime
+# from time import asctime
+#
+# a = asctime()
+# print(a)
 
-a = asctime()
-print(a)
+# task 27 When is Easter?
+# from math import floor
+#
+# year = int(input("Enter the year you're interested in: "))
+#
+# a = year / 19
+# b = floor(year / 100)
+# c = year % 100
+# d = floor(b / 4)
+# e = b % 4
+# f = floor((b + 8) / 25)
+# g = floor((b - f - 1) / 3)
+# h = (19 * a + b - d - g + 15) % 30
+# i = floor(c / 4)
+# k = c % 4
+# l = (32 + 2 * e + 2 * i - h - k) % 7
+# m = floor((a + 11 * h + 22 * l) / 451)
+# month = int(h + l + 7 * m + 114) / 31
+# day = int(1 + (h + l - 7 * m +114) % 31)
+# print("Easter in year ", year, "will be at %.0f %.0f" % (day, month))
+
+# task 28 Body Mass Index
+
+# weight = float(input("Enter your weight, kilo: "))
+# height = float(input("Enter your height, cm: "))
+#
+# bmi = weight / (height ** 2)
+#
+# print("Your body mass index is %.2f" % bmi)
+
+# task 29 Temperature taking into account wind
+
+# WC_OFFSET = 13.12
+# WC_FACTOR1 = 0.6215
+# WC_FACTOR2 = -11.37
+# WC_FACTOR3 = 0.3965
+# WC_EXPONENT = 0.16
+#
+# temp = float(input("Temperature, Celsius degrees: "))
+# speed = float(input("Wind speed, km/h: "))
+#
+# wci = WC_OFFSET + \
+# WC_FACTOR1 * temp + \
+# WC_FACTOR2 * speed ** WC_EXPONENT + \
+# WC_FACTOR3 * temp * speed ** WC_EXPONENT
+# print("Коэффициент охлаждения ветром равен", round(wci))
+
+# task 30 Pressure units
+#
+# kpa = float(input("Enter your value in kPa: "))
+# psi = kpa / 6.895
+#
+# print("Your value in psi is %.2f" % psi)
+
+# task 31 Celsius to Fahrenheit and Kelvin
+
+# the same as task 30
+
+# task 32 The sum of digits in a number
+
+# a = int(input("Your number, please: "))
+# b = a % 10
+# c = a // 10 % 10
+# d = a // 100 % 10
+# e = a // 1000
+# print(b + c + d + e)
+
+# task 33 Sorting three numbers
+
+# a = int(input("Number 1: "))
+# b = int(input("Number 2: "))
+# c = int(input("Number 3: "))
+#
+# nmax = max(a, b, c)
+# nmin = min(a, b, c)
+# medium = a + b + c - nmax - nmin
+# print("Числа в порядке возрастания:")
+# print(" ", nmin)
+# print(" ", medium)
+# print(" ", nmax)
+
+# task 34 Yesterday's bread
+
+# BREAD_PRICE = 3.49
+# DISCOUNT_RATE = 0.60
+#
+# num_loaves = int(input("Enter number of yesterday's loaves: "))
+# regular_price = num_loaves * BREAD_PRICE
+# discount = regular_price * DISCOUNT_RATE
+# total = regular_price - discount
+#
+# print("Nominal price: %5.2f" % regular_price)
+# print("Discount: %5.2f" % discount)
+# print("Total: %5.2f" % total)
